37b5aae88bb77f5e69309d78c2bdf7f74c505395
Frederick (Fred) T. A. Freeth <114069006+FreddieTAFreeth@users.noreply.github.com>
2023-10-25 16:40:38 +0100

    Update app.R

--- app.R
+++ app.R
@@ -17,18 +17,42 @@ ui <- fluidPage(
   ),
   textInput(
     inputId = "reviewer_titles_and_affiliations",
-    label = "Reviewer Titles and Affiliations",
+    label = "Reviewer Titles and Affiliations:",
     placeholder = "Please insert your titles and affiliations here."
   ),
   textInput(
     inputId = "reviewer_familiarity",
-    label = "Please describe your familiarity with the tool.",
+    label = "Please describe your familiarity with the tool:",
     placeholder = "Please describe your familiarity here."
   ),
   textInput(
     inputId = "tool_name",
-    label = "Name of Tool",
+    label = "Name of Tool:",
     placeholder = "Please insert the name of the tool."
+  ),
+  selectInput(
+    inputId = "tool_types",
+    label = "Type(s) of tool:",
+    choices = c("Prioritization", "Assessment", "Planning", "Implementation", "Evaluation"),
+    multiple = TRUE,
+    selectize = TRUE
+  ),
+  selectInput(
+    inputId = "tool_objectives",
+    label = "Objective(s):",
+    choices = c("Data Collection", "Translation", "Validation", "Analysis", "Reporting"),
+    multiple = TRUE,
+    selectize = TRUE
+  ),
+  textInput(
+    inputId = "tool_source",
+    label = "Tool Source:",
+    placeholder = "Please insert the source of the tool."
+  ),
+  textInput(
+    inputId = "tool_version",
+    label = "Version Number of the Tool:",
+    placeholder = "Please insert the version number of the tool."
   ),
 )
 
