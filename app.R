cb644a36bf5b4328a4687a8c659e3d7d9f115f4c
Frederick (Fred) T. A. Freeth <114069006+FreddieTAFreeth@users.noreply.github.com>
2023-10-25 16:13:54 +0100

    Initial fields.

--- app.R
+++ app.R
@@ -3,8 +3,40 @@
 # United Against Rabies Forum WG1 Tool - Frederick T. A. Freeth     25/10/2023 |
 # ============================================================================ #
 
+# Main dark blue colour: #1a3146
+# pink/red red color: #ff6960
+# light blue/silver: #6e818f
 
+library(shiny)
 
+ui <- fluidPage(
+  textInput(
+    inputId = "reviewer_names",
+    label = "Reviewer Name(s)",
+    placeholder = "Please insert your name(s) here."
+  ),
+  textInput(
+    inputId = "reviewer_titles_and_affiliations",
+    label = "Reviewer Titles and Affiliations",
+    placeholder = "Please insert your titles and affiliations here."
+  ),
+  textInput(
+    inputId = "reviewer_familiarity",
+    label = "Please describe your familiarity with the tool.",
+    placeholder = "Please describe your familiarity here."
+  ),
+  textInput(
+    inputId = "tool_name",
+    label = "Name of Tool",
+    placeholder = "Please insert the name of the tool."
+  ),
+)
+
+server <- function(input, output, session) {
+  
+}
+
+shinyApp(ui, server)
 
 # ============================================================================ #
 # United Against Rabies Forum WG1 Tool - Code End                              |
