164d0179ab9cfdbea62b00985dd00d5f9193bba2
Frederick (Fred) T. A. Freeth <114069006+FreddieTAFreeth@users.noreply.github.com>
2023-10-25 20:41:51 +0100

    Logo is up; minor spell checks.

--- app.R
+++ app.R
@@ -18,6 +18,8 @@ preloaded_languages <- c(
 
 
 ui <- fluidPage(
+  HTML('<center><a href="https://www.unitedagainstrabies.org/"><img src="uarf_logo_web.svg", style="width:250px;"></a></center>'),
+  p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
   textInput(
     inputId = "reviewer_names",
     label = "Reviewer Name(s)",
@@ -70,7 +72,7 @@ ui <- fluidPage(
   textInput(
     inputId = "tool_availability",
     label = "Version Number of the Tool:",
-    placeholder = "Please descrbe the availability of the tool."
+    placeholder = "Please describe the availability of the tool."
   ),
   selectInput( # need to be able to input the number of countries if # countries, if known is selected
     inputId = "tool_history_of_use",
@@ -91,7 +93,11 @@ ui <- fluidPage(
       "persist" = TRUE
     )
   ),
-  
+  textInput(
+    inputId = "tool_description",
+    label = "Brief Tool Description",
+    placeholder = "Please describe the tool briefly here."
+  )
 )
 
 server <- function(input, output, session) {
